e9c983c2df5804f32cccaf72bfae9194e0d2b822
Nicolas Ganivet <nicolas@cividesk.com>
2013-04-29 15:56:14 -0700

    Initial port - basic features, no UI for settings. Redirect and email are working!

--- reporterror.php
+++ reporterror.php
@@ -3,7 +3,7 @@
 require_once 'reporterror.civix.php';
 
 define('REPORTERROR_CIVICRM_SUBJECT_LEN', 100);
-define('REPORTERROR_SETTINGS_GROUP', 'reporterror');
+define('REPORTERROR_SETTINGS_GROUP', 'ReportError Extension');
 
 /**
  * Implementation of hook_civicrm_config
@@ -93,26 +93,25 @@ function reporterror_civicrm_handler($vars) {
   // Try to handle the error in a more user-friendly way
   //
  
-  /*
   // Contribution forms: error with no HTTP_REFERER (most likely a bot, restored session, or copy-pasted link)
-  if (arg(0) == 'civicrm' && arg(1) == 'contribute' && arg(2) == 'transact' && ! $_SERVER['HTTP_REFERER']) {
-    $handle = variable_get('civicrm_error_fallback_contrib_noreferer_handle', 0);
-    $pageid = variable_get('civicrm_error_fallback_contrib_noreferer_dest', 0);
+  $config = CRM_Core_Config::singleton();
+  $urlVar = $config->userFrameworkURLVar;
+  $arg = explode('/', $_GET[$urlVar]);
+  // Redirect for Contribution pages without a referrer (close / restore browser page)
+  if ($arg[0] == 'civicrm' && $arg[1] == 'contribute' && $arg[2] == 'transact' && ! $_SERVER['HTTP_REFERER']) {
+    $handle = CRM_Core_BAO_Setting::getItem(REPORTERROR_SETTINGS_GROUP, 'noreferer_handle');
+    $pageid = CRM_Core_BAO_Setting::getItem(REPORTERROR_SETTINGS_GROUP, 'noreferer_pageid');
+    $sendreport = CRM_Core_BAO_Setting::getItem(REPORTERROR_SETTINGS_GROUP, 'noreferer_pageid', NULL, 1);
 
     if ($handle == 1 || ($handle == 2 && ! $pageid)) {
-      $redirect_path = '<front>';
+      $redirect_path = CRM_Utils_System::baseCMSURL();
     }
     elseif ($handle == 2) {
-      $redirect_path = 'civicrm/contribute/transact';
-      $redirect_options['query'] = array(
-        'reset' => 1,
-        'id' => $pageid,
-      );
+      $redirect_path = CRM_Utils_System::url('civicrm/contribute/transact', 'reset=1&id='.$pageid);
     }
-
-    $sendreport = variable_get('civicrm_error_fallback_contrib_noreferer_alert', 1);
   }
 
+  /*
   if (arg(0) == 'civicrm' && arg(1) == 'contribute' && arg(2) == 'campaign' && ! isset($_REQUEST['component'])) {
     $handle = variable_get('civicrm_error_fallback_pcp_nocomponent_handle', 0);
     $pageid = $_REQUEST['pageId'];
@@ -153,7 +152,6 @@ function reporterror_civicrm_handler($vars) {
     }
   
     $to = CRM_Core_BAO_Setting::getItem(REPORTERROR_SETTINGS_GROUP, 'mailto');
-dsm($to, 'to');
 
     if (! $to) {
       // FIXME: log to error log?
@@ -161,11 +159,11 @@ dsm($to, 'to');
     }
 
     $destinations = explode(',', $to);
-    $output = civicrm_error_generatereport($vars, $redirect_path, $redirect_options);
+    $output = reporterror_civicrm_generatereport($site_name, $vars, $redirect_path);
   
     foreach ($destinations as $dest) {
       $dest = trim($dest);
-      civicrm_error_send_mail($dest, $subject, $output);
+      reporterror_civicrm_send_mail($dest, $subject, $output);
     }
   }
 
@@ -173,6 +171,124 @@ dsm($to, 'to');
   if ($redirect_path) {
     // 307 = temporary redirect. Assuming it reduces the chances that the browser
     // keeps the redirection in cache.
-    drupal_goto($redirect_path, $redirect_options, 307);
+    CRM_Utils_System::redirect($redirect_path);
   }
 }
+
+/**
+ * Returns a plain text output for the e-mail report.
+ */
+function reporterror_civicrm_generatereport($site_name, $vars, $redirect_path) {
+  $output = ts('There was a CiviCRM error at %1.', array(1 => $site_name)) . "\n";
+  $output .= ts('Date: %1', array(1 => date('c'))) . "\n\n";
+
+  if ($redirect_path) {
+    $output .= ts("Error handling rules redirected the user to:") . "\n";
+    $output .= $redirect_path . "\n\n";
+  }
+
+  // Error details
+  if (function_exists('error_get_last')) {
+    $output .= "***ERROR***\n";
+    $output .= print_r(error_get_last(), TRUE);
+  }
+
+  $output .= print_r($vars, TRUE);
+
+  // User info
+  $session = CRM_Core_Session::singleton();
+  $userId = $session->get('userID');
+  $params = array(
+    'version' => 3,
+    'id' => $userId,
+    'return' => 'id,display_name,email',
+  );
+  $contact = civicrm_api('Contact', 'getsingle', $params);
+  $output .= "\n\n***LOGGED IN USER***\n";
+  $output .= _reporterror_civicrm_parse_array($contact);
+
+  // $_SERVER
+  $output .= "\n\n***SERVER***\n";
+  $output .= _reporterror_civicrm_parse_array($_SERVER);
+
+  // Backtrace
+  $backtrace = debug_backtrace();
+  $output .= "\n\n***BACKTRACE***\n";
+  foreach ($backtrace as $call) {
+    $output .= "**next call**\n";
+    $output .= _reporterror_civicrm_parse_array($call);
+  }
+
+  return $output;
+}
+
+/**
+ * Send the e-mail using drupal_mail()
+ */
+function reporterror_civicrm_send_mail($to, $subject, $output) {
+    if ($domain_id = CRM_Core_Config::domainID()) {
+      // Gather information from domain settings
+      $params = array('id' => $domain_id);
+      CRM_Core_BAO_Domain::retrieve($params, $domain);
+      unset($params['id']);
+      $locParams = $params + array('entity_id' => $domain_id, 'entity_table' => 'civicrm_domain');
+      $defaults = CRM_Core_BAO_Location::getValues($locParams);
+      $email_struct = reset(CRM_Utils_Array::value('email', $defaults));
+      $email = CRM_Utils_Array::value('email', $email_struct);
+    }
+    if (!$email) { // FIXME: Just in case ...
+      $email = 'nobody@nowhere.com';
+    }
+
+    $params = array(
+    'from' => $email,
+    'toName' => 'Site Administrator',
+    'toEmail' => $to,
+    'subject' => $subject,
+    'text' => $output,
+  );
+
+  if (!CRM_Utils_Mail::send($params)) {
+    //FIXME: Output an error message to log
+  }
+}
+
+/**
+ *  Helper function to return a pretty print of the given array
+ *
+ *  @param array $array
+ *    The array to print out.
+ *  @return string
+ *    The printed array.
+ */
+function _reporterror_civicrm_parse_array($array) {
+  $output = '';
+
+  foreach ((array)$array as $key => $value) {
+    if (is_array($value) || is_object($value)) {
+      $value = print_r($value, TRUE);
+    }
+    $key = str_pad($key .':', 20, ' ');
+    $output .= $key . (string)_reporterror_civicrm_check_length($value) ." \n";
+  }
+  return $output ."\n";
+}
+
+/**
+ *  Helper function to add elipses and return spaces if null
+ *
+ *  @param string $item
+ *    String to check.
+ *  @return string
+ *    The truncated string.
+ */
+function _reporterror_civicrm_check_length($item) {
+  if (is_null($item)) {
+    return ' ';
+  }
+  if (strlen($item) > 2000) {
+    $item = substr($item, 0, 2000) .'...';
+  }
+  return $item;
+}
+
