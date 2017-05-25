49d67b83a2e45eb9242fa898d82b9235ea6449db
Mathieu Lutfy <mlutfy@koumbit.org>
2017-05-25 11:30:59 -0400

    CRM/Admin/Form/Setting/ReportError.php: remove (C) notice (not relevant/accurate).

--- CRM/Admin/Form/Setting/ReportError.php
+++ CRM/Admin/Form/Setting/ReportError.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
- +--------------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2012-2013                                      |
- +--------------------------------------------------------------------------+
- | This program is free software: you can redistribute it and/or modify     |
- | it under the terms of the GNU Affero General Public License as published |
- | by the Free Software Foundation, either version 3 of the License, or     |
- | (at your option) any later version.                                      |
- |                                                                          |
- | This program is distributed in the hope that it will be useful,          |
- | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
- | GNU Affero General Public License for more details.                      |
- |                                                                          |
- | You should have received a copy of the GNU Affero General Public License |
- | along with this program.  If not, see <http://www.gnu.org/licenses/>.    |
- +--------------------------------------------------------------------------+
-*/
-
 class CRM_Admin_Form_Setting_ReportError extends CRM_Admin_Form_Setting {
   protected $_values;
 
@@ -180,4 +161,3 @@ class CRM_Admin_Form_Setting_ReportError extends CRM_Admin_Form_Setting {
     CRM_Core_Session::setStatus(ts('Settings saved.', array('domain' => 'ca.bidon.reporterror')), '', 'success');
   }
 }
-
