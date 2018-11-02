a484fb1c165970f2d0af80222a75e27d71881e31
Mathieu Lutfy <mathieu@bidon.ca>
2018-11-02 10:57:59 -0400

    CRM/ReportError/Handler/Profiles: do not return TRUE unless we redirected (causes a blank page otherwise). More variable/notices fixes.

--- CRM/ReportError/Handler/Profiles.php
+++ CRM/ReportError/Handler/Profiles.php
@@ -16,23 +16,20 @@ class CRM_ReportError_Handler_Profiles {
     $arg = explode('/', $_GET[$urlVar]);
 
     if ($arg[0] == 'civicrm' && $arg[1] == 'profile') {
-      $redirect = reporterror_setting_get('reporterror_noreferer_handle_profiles', $options_overrides);
+      $handle = reporterror_setting_get('reporterror_noreferer_handle_profiles', $options_overrides);
       $sendreport = reporterror_setting_get('reporterror_sendreport_profile', $options_overrides);
 
       $vars['redirect_path'] = CRM_Utils_System::baseCMSURL();
-
-      $output = reporterror_civicrm_generatereport($site_name, $vars, NULL, $options_overrides);
 
       if ($sendreport) {
         CRM_ReportError_Utils::sendReport($vars, $options_overrides);
       }
 
       if ($handle && !empty($vars['redirect_path'])) {
-        Civi::log()->warning("Report Error Extension: redirected to home page:\n" . $output);
+        Civi::log()->warning("Report Error Extension: redirected to home page: " . print_r($arg, 1));
         CRM_Utils_System::redirect($vars['redirect_path']);
+        return TRUE;
       }
-
-      return TRUE;
     }
 
     return FALSE;
